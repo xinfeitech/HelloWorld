4bb17449d4e6506e94e40992bc6d0c3779370d41
xinfeitech <xinfeitech@aliyun.com>
2017-04-09 16:35:57 +0800

    Initial commit

--- app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class HelloWorldActivity extends AppCompatActivity {
 
     @Override
-    //liqiaing
+    //xinfeitech
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
